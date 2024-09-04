2e51ff6d8abdb991d77f0bcfed7afd039699463a
saleh shokouhi <pgulf.611@gmai.com>
2024-09-04 15:07:19 +0330

    feat: clone module

--- addModule.sh
+++ addModule.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# Check for the --just-clone option
+just_clone=false
+if [ "$1" == "--just-clone" ]; then
+    just_clone=true
+fi
+
 # 1. Get the module name from the user
 read -p "Enter the module name: " module_name
 
@@ -14,15 +20,31 @@ fi
 read -p "Enter the git repository address: " git_repo
 
 # 4. Create the directory under 'layers'
-# mkdir -p "./layers/$module_name"
-# 11. Add the Git submodule
-git submodule add -f "$git_repo" "./layers/$module_name" 
+cd "./layers"
+git clone "$git_repo"
 
+# 5. Navigate to the cloned repository directory
+cd "$module_name"
 
-# 5. Create 'locales' directory under the module directory
+# 6. Check if the 'develop' branch exists
+if git rev-parse --verify develop &>/dev/null; then
+    # If 'develop' branch exists, switch to it
+    git checkout develop
+else
+    # If 'develop' branch does not exist, create and switch to it
+    git checkout -b develop
+fi
+
+# If --just-clone option is provided, exit after cloning and switching/creating the branch
+if [ "$just_clone" = true ]; then
+    echo "Repository '$git_repo' has been cloned and switched to the 'develop' branch. Exiting as per --just-clone option."
+    exit 0
+fi
+
+# 7. Create 'locales' directory under the module directory
 mkdir -p "./layers/$module_name/locales"
 
-# 6. Iterate over files in 'libs/i18n/locales' and create files under 'locales' with predefined content
+# 8. Iterate over files in 'libs/i18n/locales' and create files under 'locales' with predefined content
 for file in ./libs/i18n/locales/*; do
     filename=$(basename "$file")
     echo "export default {
@@ -30,7 +52,7 @@ for file in ./libs/i18n/locales/*; do
     };" > "./layers/$module_name/locales/$filename"
 done
 
-# 7. Create 'nuxt.config.ts' under the module directory
+# 9. Create 'nuxt.config.ts' under the module directory
 echo "// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -51,22 +73,20 @@ echo "  ],
   },
 });" >> "./layers/$module_name/nuxt.config.ts"
 
-# 8. Create 'pages' directory under the module directory
+# 10. Create 'pages' directory under the module directory
 mkdir -p "./layers/$module_name/pages"
 
-# 12. Initialize Git Flow with default configurations
+# 11. Initialize Git Flow with default configurations
 cd layers/$module_name || exit
 
-# 9. Run 'pnpm init' inside the module directory
+# 12. Run 'pnpm init' inside the module directory
 pnpm init 
 
-# 10. Install Husky and initialize it, then copy hooks from the main project
+# 13. Install Husky and initialize it, then copy hooks from the main project
 pnpm add --save-dev husky
 
 cp -ra $SCRIPTPATH/.husky/. $SCRIPTPATH/layers/$module_name/.husky/
 
 pnpm exec husky init
-
-
 
 echo "Module '$module_name' successfully created and set up with Nuxt3."
