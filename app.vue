425a9c8b19db36d159df47af8beef45d05ab039b
saleh shokouhi <pgulf.611@gmai.com>
2024-09-08 17:13:35 +0330

    fix: add rtl/ltr base on lang

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <v-app>
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
+      <v-locale-provider :rtl="localeProperties.dir === 'rtl'">
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+      </v-locale-provider>
     </v-app>
   </div>
 </template>
+
+<script setup lang="ts">
+const { localeProperties } = useI18n()
+</script>
