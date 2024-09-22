f59f15197170597062684b72fbb59cbb2b239f63
bita meshgi <meshgibita@gmail.com>
2024-09-22 14:08:44 +0330

    feat: add rules for register form

--- libs/form/rules.ts
+++ libs/form/rules.ts
@@ -1,3 +1,5 @@
+
+const password = ref('')
 export const isEmail = (value: string) => {
     if (!value) return 'Email is required'
     if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(value)) return true
@@ -7,4 +9,19 @@ export const isPassword = (value: string) => {
     if (!value) return 'Password is required'
     if (value.length < 8) return 'Password must be at least 8 characters long'
     return true
+}
+export const noZeroStater = (value: string) => {
+    if (!value) return 'Value is required'
+    if (/^0/.test(value)) return 'Value must not start with zero'
+    return true
+}
+export const passwordConfirmation = (value: string, password: string) => {
+    if (!value) return 'Confirmation password is required'
+    if (value !== password) {
+        return 'Passwords do not match'
+    }
+    return true
+}
+export const isVerificationCode = (value: string) => {
+    if (!value) return 'Verification code is required'
 }
